3643b881cfa5708fbf1dc81345a64ea8a9fbb973
GalantKac <kacper21galant@gmail.com>
2020-08-11 19:24:21 +0200

    Create Game and Player Specification class

---
+++ Data/Specification/GameSpecification.cs
@@ -0,0 +1,15 @@
+﻿using Football.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Football.Data.Specification
+{
+    public class GameSpecification : BaseSpecification<Game>
+    {
+        public GameSpecification(int id): base(item => item.Id == id) { }
+        public GameSpecification(int homePlayerId, int awayPlayerId): base(item => item.HomePlayerId == homePlayerId && item.AwayPlayerId == awayPlayerId) { }
+        public GameSpecification(DateTime date): base(item => item.GameDate == date) { }
+    }
+}
---
+++ Data/Specification/PlayerSpecification.cs
@@ -0,0 +1,15 @@
+﻿using Football.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Football.Data.Specification
+{
+    public class PlayerSpecification : BaseSpecification<Player>
+    {
+        public PlayerSpecification(int id) : base(item => item.Id == id) { }
+
+        public PlayerSpecification(string name, string password): base(item=> item.Name == name && item.Password == password) { }
+    }
+}
