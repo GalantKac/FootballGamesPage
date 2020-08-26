23e107337f04f8a8b677dd10742ea91a6b5f9cc7
GalantKac <kacper21galant@gmail.com>
2020-08-26 20:27:02 +0200

    Created IUnitOfWork interface

---
+++ Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Football.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Football.Interfaces
+{
+    interface IUnitOfWork
+    {
+        IRepository<Game> Games { get; }
+        IRepository<Player> Players { get; }
+        void Save();
+    }
+}
